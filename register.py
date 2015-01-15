2e49a1458c2fc9bc02835256dc993e6e2f15be4d
Alexis Tremblay <alexis.tremblay@gmail.com>
2015-01-15 07:26:19 -0500

    Started implementation to register series and donwload covers

--- register.py
+++ register.py
@@ -1,21 +1,53 @@
 import sqlite3
 import argparse
+from series import Series
 
-"""
-CREATE TABLE series(
-serieid INTEGER PRIMARY KEY,
-seriename TEXT,
-serieurl TEXT);
+database_name = "dcwikia.db"
 
-CREATE TABLE comics(
-comicid INTEGER PRIMARY KEY,
-comicurl TEXT,
-serieid INTEGER,
-FOREIGN KEY(serieid) REFERENCES series(serieid));
+def parse_args():
+    parser = argparse.ArgumentParser()
 
-CREATE TABLE covers(
-coverid INTEGER PRIMARY KEY,
-coverurl TEXT,
-comicid INTEGER,
-FOREIGN KEY (comicid) REFERENCES comics(comicid));
-"""
+    parser.add_argument('-r', '--register', help='Register a new series in '
+        'the database. You must provide the url from dcwikia')
+    parser.add_argument('-d', '--download', help='Download all new covers')
+
+def create_tables():
+    conn = sqlite3.Connection(database_name)
+    cur = conn.cursor()
+
+    queries = ["""CREATE TABLE series(
+        serieid INTEGER PRIMARY KEY,
+        serieurl TEXT UNIQUE);
+        """, """CREATE TABLE comics(
+        comicid INTEGER PRIMARY KEY,
+        comicurl TEXT,
+        serieid INTEGER,
+        FOREIGN KEY(serieid) REFERENCES series(serieid));
+        """, """CREATE TABLE covers(
+        coverid INTEGER PRIMARY KEY,
+        coverurl TEXT,
+        comicid INTEGER,
+        FOREIGN KEY (comicid) REFERENCES comics(comicid));
+        """]
+
+    for query in queries:
+        cur.execute(query)
+
+def add_series(url):
+    query = """INSERT INTO series VALUES(?)"""
+    conn = sqlite3.Connection(database_name)
+    cur = conn.cursor()
+
+    cur.execute(query, url)
+
+def download():
+    query = """SELECT serieurl FROM series"""
+    conn = sqlite3.Connection(database_name)
+    cur = conn.cursor()
+    cur.execute(query)
+    urls = cur.fetchall()
+    for url in urls:
+        series = Series(url[0])
+        for comic in series.comics:
+            comic.download_covers()
+
